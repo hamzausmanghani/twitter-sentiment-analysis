69b5ce26e15f006b03ab05022e916b1420a8674c
Hamza Usman ghani <hamzausmanghani@gmail.com>
2022-03-31 01:59:59 +0500

    Changing port

--- test.py
+++ test.py
@@ -1,21 +1,20 @@
 import json
 import pandas as pd
 import requests
-# from API import *
 
 
-# rsp = requests.get(url="http://127.0.0.1:30000/get_tweets",
-#                    headers={"apiKey": "IJHXvVKglhWypDgq12mQWm2lp"})
-# print("all", rsp.status_code, rsp.content)
-# if rsp.status_code == 200:
-#     df = pd.DataFrame(json.loads(rsp.content))
-#     df.to_csv("tweets.csv", index=False)
+rsp = requests.get(url="http://127.0.0.1:30000/get_tweets",
+                   headers={"apiKey": "IJHXvVKglhWypDgq12mQWm2lp"})
+print("all", rsp.status_code)
+if rsp.status_code == 200:
+    df = pd.DataFrame(json.loads(rsp.content))
+    df.to_csv("tweets.csv", index=False)
 
 
 rsp = requests.get(url="http://127.0.0.1:30000/search_by",
                    headers={"apiKey": "IJHXvVKglhWypDgq12mQWm2lp"},
                    params={"tag": "bitcoin"})
-print("search", rsp.status_code, rsp.content)
+print("search", rsp.status_code)
 if rsp.status_code == 200:
     df = pd.DataFrame(json.loads(rsp.content))
     df.to_csv("tag_search.csv", index=False)
