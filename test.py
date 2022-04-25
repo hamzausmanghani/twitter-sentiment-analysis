7ab685ce5f969fc009bc65cf9675a386fa4939b4
Hamza Usman Ghani <hamza.usman@REDOM.COM>
2022-04-25 22:41:03 +0500

    Reformatted a bit

--- test.py
+++ test.py
@@ -9,37 +9,37 @@ port = "30000"
 api_key = "IJHXvVKglhWypDgq12mQWm2lp"
 verbose = True
 
-# serach filters
-tag = "bitcoin"
-start = "2022-03-30"
-end = "2022-03-31"
 
-
-def get_all_tweets(fileName="tweets"):
+def get_all_tweets(start, end):
     rsp = requests.get(url=f"http://{server_ip}:{port}/get_tweets",
                        headers={"apiKey": api_key},
                        params={"start": start, "end": end})
     if rsp.status_code == 200:
-        df = pd.DataFrame(json.loads(rsp.content))
-        df.to_csv(f"{fileName}-{start}-{end}.csv", index=False)
-        if verbose: print(f"Data fetched and stored in file named '{fileName}'")
+        df = pd.DataFrame(json.loads(rsp.content)['data'])
+        file = f"tweets-{start}-{end}.csv"
+        df.to_csv(file, index=False)
+        if verbose: print(f"Data fetched and stored in file named '{file}'")
     else:
         if verbose: print(f"Error in fetching data: status <{rsp.status_code}>")
 
 
-def get_tweets_by_tag(tag, fileName="tag_search"):
+def get_tweets_by_tag(tag, start, end):
     rsp = requests.get(url=f"http://{server_ip}:{port}/search_by",
                        headers={"apiKey": api_key},
                        params={"tag": tag, "start": start, "end": end})
     if rsp.status_code == 200:
-        df = pd.DataFrame(json.loads(rsp.content))
-        df.to_csv(f"{fileName}-{tag}-{start}-{end}.csv", index=False)
-        if verbose: print(f"Data fetched and stored in file named '{fileName}'")
+        df = pd.DataFrame(json.loads(rsp.content)['data'])
+        file = f"{tag}-tweets-{start}-{end}.csv"
+        df.to_csv(file, index=False)
+        if verbose: print(f"Data fetched and stored in file named '{file}'")
     else:
         if verbose: print(f"Error in fetching data: status <{rsp.status_code}>")
 
 
 if __name__ == '__main__':
-    get_all_tweets(fileName="all_tweets")
-    get_tweets_by_tag(tag=tag, fileName="bitcoin_tweets")
+    tag = "bitcoin"
+    start = "2022-04-10"
+    end = "2022-04-15"
+    get_all_tweets(start, end)
+    get_tweets_by_tag(tag, start, end)
 
